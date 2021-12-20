7ccb7957a8b493181d19d3538e544aece47a524a
Stilic <lateman-jpeg@outlook.fr>
2021-12-20 22:27:34 +0100

    base work

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>JeckSpace</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <div class="top">
+            <div>
+                <p><a href="/">JeckSpace.Com</a> &nbsp;|&nbsp; <a href="/">Home</a></p>
+            </div>
+            <div class="right">
+                <p>Search JeckSpace:&nbsp;</p>
+                <input type="text">
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
