a9800fdfa23d9938ad21e8682531f2e236c5e541
Stilic <lateman-jpeg@outlook.fr>
2021-12-21 21:22:26 +0100

    fatadd

--- index.php
+++ index.php
@@ -29,8 +29,94 @@
         </div>
     </header>
     <nav class="navbar">
-        <a href="#">Home</a> | <a href="#">Browse</a> | <a href="#">Search</a> | <a href="#">Invite</a> | <a href="#">Invite</a> | <a href="#">Mail</a> | <a href="#">Blog</a> | <a href="#">Favorites</a> | <a href="#">Forum</a> | <a href="#">Groups</a> | <a href="#">Games</a> | <a href="#">Music</a> | <a href="#">Classifieds</a> | <a href="#">SignUp</a>
+        <a href="#">Home</a>
+        |
+        <a href="#">Browse</a>
+        | <a href="#">Search</a>
+        | <a href="#">Invite</a>
+        | <a href="#">Invite</a>
+        | <a href="#">Mail</a>
+        | <a href="#">Blog</a>
+        | <a href="#">Favorites</a>
+        | <a href="#">Forum</a>
+        | <a href="#">Groups</a>
+        | <a href="#">Games</a>
+        | <a href="#">Music</a>
+        | <a href="#">Classifieds</a>
+        | <a href="#">SignUp</a>
     </nav>
+    <main>
+        <div class="mgb">
+            <section class="left">
+                <div class="ftred">
+                    <div class="title">
+                        <p>Cool New People</p>
+                    </div>
+                    <div class="content imgtable">
+                        <div>
+                            <p><a href="#">Nikki</a></p><img src="amogus.jpg" alt="">
+                        </div>
+                        <div>
+                            <p><a href="#">Haley</a></p><img src="amogus.jpg" alt="">
+                        </div>
+                        <div>
+                            <p><a href="#">Leiah</a></p><img src="amogus.jpg" alt="">
+                        </div>
+                        <div>
+                            <p><a href="#">Rachel</a></p><img src="amogus.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="ftred blue">
+                    <div class="title">
+                        <p>MySpace Member Blogs</p>
+                        <p class="fa-ri"><a href="#">[what's this?]</a></p>
+                    </div>
+                    <div class="content txttable">
+                        <div class="left">
+                            <p>02:01a - <a href="#">KadeJoe</a></p>
+                            <p>02:01a - <a href="#">KadeJoe</a></p>
+                            <p>02:01a - <a href="#">KadeJoe</a></p>
+                            <p>02:01a - <a href="#">KadeJoe</a></p>
+                            <p>02:01a - <a href="#">KadeJoe</a></p>
+                            <p>02:01a - <a href="#">KadeJoe</a></p>
+                        </div>
+                        <div class="right">
+                            <p>02:01a - <a href="#">KadeJoe</a></p>
+                            <p>02:01a - <a href="#">KadeJoe</a></p>
+                            <p>02:01a - <a href="#">KadeJoe</a></p>
+                            <p>02:01a - <a href="#">KadeJoe</a></p>
+                            <p>02:01a - <a href="#">KadeJoe</a></p>
+                            <p>02:01a - <a href="#">KadeJoe</a></p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="right">
+                <div class="login-form">
+                    <h4>Member Login</h4>
+                    <form>
+                        <label for="email">E-Mail:</label>
+                        <input type="email" name="email">
+                        <br>
+                        <br>
+                        <label for="password">Password:</label>
+                        <input type="password" name="password">
+                        <br>
+                        <br>
+                        <input type="checkbox" name="remember">
+                        <label for="remember">Remember my E-mail</label>
+                        <br>
+                        <br>
+                        <input type="submit" value="LOGIN">
+                        <br>
+                        <a href="#">Forgot your password?</a>
+                    </form>
+                    <br>
+                </div>
+            </section>
+        </div>
+    </main>
 </body>
 
 </html>
