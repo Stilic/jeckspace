413f79b0dc8b3994199e00ad955f204af22db391
Stilic <lateman-jpeg@outlook.fr>
2021-12-21 18:59:13 +0100

    kinda looks like a clone :troll:

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>JeckSpace</title>
+    <title>JeckSpace - a place for triends</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
@@ -10,16 +10,27 @@
 
 <body>
     <header>
-        <div class="top">
+        <div class="top flex">
             <div>
-                <p><a href="/">JeckSpace.Com</a> &nbsp;|&nbsp; <a href="/">Home</a></p>
+                <p><a href="/">JeckSpace.Cum</a> &nbsp;|&nbsp; <a href="/">Home</a></p>
             </div>
-            <div class="right">
+            <div class="right flex">
                 <p>Search JeckSpace:&nbsp;</p>
                 <input type="text">
+                &nbsp;&nbsp;&nbsp;
+                <button>Go</button>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <p><strong><a href="#">Help</a></strong> | <strong><a href="#">Sign Up</a></strong>
+                </p>
             </div>
         </div>
+        <div class="logo-container">
+            <img src="img/logo.png" alt="">
+        </div>
     </header>
+    <nav class="navbar">
+        <a href="#">Home</a> | <a href="#">Browse</a> | <a href="#">Search</a> | <a href="#">Invite</a> | <a href="#">Invite</a> | <a href="#">Mail</a> | <a href="#">Blog</a> | <a href="#">Favorites</a> | <a href="#">Forum</a> | <a href="#">Groups</a> | <a href="#">Games</a> | <a href="#">Music</a> | <a href="#">Classifieds</a> | <a href="#">SignUp</a>
+    </nav>
 </body>
 
 </html>
