54396b566cc71cb34f9ac60fc7166aeb49da24c5
Stilic <lateman-jpeg@outlook.fr>
2021-12-22 09:49:53 +0100

    da router + da pages + amogus

---
+++ components/login.php
@@ -0,0 +1,21 @@
+<div class="login-form">
+    <h4>Member Login</h4>
+    <form>
+        <label for="email">E-Mail:</label>
+        <input type="email" name="email">
+        <br>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" name="password">
+        <br>
+        <br>
+        <input type="checkbox" name="remember">
+        <label for="remember">Remember my E-mail</label>
+        <br>
+        <br>
+        <div class="btns">
+            <input type="submit" value="LOGIN">
+            <button class="org">SIGN UP!</button>
+        </div>
+        <a href="#">Forgot your password?</a>
+</div>
--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html>
 
-<!-- https://web.archive.org/web/20040610090214/http://www.myspace.com/index.cfm?fuseaction=splash -->
-
 <head>
     <title>JeckSpace.cum - a place for triends</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style.css">
 </head>
 
 <body>
@@ -28,11 +26,11 @@
                 </div>
             </div>
             <div class="logo-container">
-                <img src="img/logo.png" alt="">
+                <img src="/img/logo.png" alt="">
             </div>
         </header>
         <nav>
-            <a href="#">Home</a>
+            <a href="/">Home</a>
             | <a href="#">Browse</a>
             | <a href="#">Search</a>
             | <a href="#">Invite</a>
@@ -48,129 +46,46 @@
             | <a href="#">SignUp</a>
         </nav>
         <main>
-            <div class="mgb">
-                <section class="left">
-                    <div class="featured">
-                        <div class="title">
-                            <p>Cool New People</p>
-                        </div>
-                        <div class="content imgtable">
-                            <div>
-                                <a href="#">
-                                    <p>Nikki</p><img src="img/amogus.jpg" alt="">
-                                </a>
-                            </div>
-                            <div>
-                                <a href="#">
-                                    <p>Haley</p><img src="img/amogus.jpg" alt="">
-                                </a>
-                            </div>
-                            <div>
-                                <a href="#">
-                                    <p>Leiah</p><img src="img/amogus.jpg" alt="">
-                                </a>
-                            </div>
-                            <div>
-                                <p><a href="#">Rachel</a></p><img src="img/amogus.jpg" alt="">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="featured blue">
-                        <div class="title">
-                            <p>MySpace Member Blogs</p>
-                            <p class="fa-ri"><a href="#">[what's this?]</a></p>
-                        </div>
-                        <div class="content txttable">
-                            <div class="left">
-                                <p>02:01a - <a href="#">KadeJoe</a></p>
-                                <p>02:01a - <a href="#">KadeJoe</a></p>
-                                <p>02:01a - <a href="#">KadeJoe</a></p>
-                                <p>02:01a - <a href="#">KadeJoe</a></p>
-                                <p>02:01a - <a href="#">KadeJoe</a></p>
-                                <p>02:01a - <a href="#">KadeJoe</a></p>
-                            </div>
-                            <div class="right">
-                                <p>02:01a - <a href="#">KadeJoe</a></p>
-                                <p>02:01a - <a href="#">KadeJoe</a></p>
-                                <p>02:01a - <a href="#">KadeJoe</a></p>
-                                <p>02:01a - <a href="#">KadeJoe</a></p>
-                                <p>02:01a - <a href="#">KadeJoe</a></p>
-                                <p>02:01a - <a href="#">KadeJoe</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <section class="right">
-                    <div class="login-form">
-                        <h4>Member Login</h4>
-                        <form>
-                            <label for="email">E-Mail:</label>
-                            <input type="email" name="email">
-                            <br>
-                            <br>
-                            <label for="password">Password:</label>
-                            <input type="password" name="password">
-                            <br>
-                            <br>
-                            <input type="checkbox" name="remember">
-                            <label for="remember">Remember my E-mail</label>
-                            <br>
-                            <br>
-                            <div class="btns">
-                                <input type="submit" value="LOGIN">
-                                <button class="org">SIGN UP!</button>
-                            </div>
-                            <a href="#">Forgot your password?</a>
-                    </div>
-                    <div class="ad">
-                        <img src="img/advertisement.gif" alt="ADVERTISSEMENT">
-                    </div>
-                    </form>
-            </div>
-            </section>
-            <div class="news">
-                <div>
-                    <section>
-                        <h3>Get Started on JeckSpace!</h3>
-                        <p>Join for free, and view profiles, connect with others, blog, rank music, and much more!</p>
-                    </section>
-                    <p class="mlink"><a href="#">Learn More</a></p>
-                </div>
-                <div>
-                    <section>
-                        <h3>Create Your Profile!</h3>
-                        <p>Tell us about yourself, upload your pictures, and start adding triends to your network!</p>
-                    </section>
-                    <p class="mlink"><a href="#">Start Now</a></p>
-                </div>
-                <div>
-                    <section>
-                        <h3>Browse through Profiles!</h3>
-                        <p>Read through millions of profiles on JeckSpace! See pix, read blogs, and more!</p>
-                    </section>
-                    <p class="mlink"><a href="#">Browse Now</a></p>
-                </div>
-                <div>
-                    <section>
-                        <h3>Invite Your Triends!</h3>
-                        <p>Invite your triends, and as they invite their triends your network will grow even larger!</p>
-                    </section>
-                    <p class="mlink"><a href="#">Invite Triends Now</a></p>
-                </div>
-            </div>
-    </div>
-    </main>
+            <?php
+            // https://web.archive.org/web/20040610090214/http://www.myspace.com/index.cfm?fuseaction=splash
+            require 'vendor/autoload.php';
+            $router = new AltoRouter();
+            function page($page)
+            {
+                require __DIR__ . '/views/' . $page . '.php';
+            }
+
+            $router->map('GET', '/', function () {
+                page('home');
+            });
+            $router->map('GET', '/login', function () {
+                page('login');
+            });
+
+            $router->map('GET', '/misc/about', function () {
+                page('about');
+            });
+
+            $match = $router->match();
+            if (is_array($match) && is_callable($match['target'])) {
+                call_user_func_array($match['target'], $match['params']);
+            } else {
+                header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+                echo '<p><strong>404 Not Found</strong></p>';
+            }
+            ?>
+        </main>
     </div>
     <div class="bg">
         <div class="bbb">
             <footer>
                 <div class="flex fce">
-                    <img src="img/truste.gif" alt="TRUSTe">
+                    <img src="/img/truste.gif" alt="TRUSTe">
                     &nbsp;
                     <p>JeckSpace is <strong>NOT</strong> a licensee of the TRUSTe Privacy Program</p>
                 </div>
                 <nav class="trans">
-                    <a href="#">About</a>
+                    <a href="/misc/about">About</a>
                     | <a href="#">News</a>
                     | <a href="#">FAQ</a>
                     | <a href="#">Terms</a>
