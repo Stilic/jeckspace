d43fc860c65bbfc77ef461ef9f3ef6962e11c6d7
Stilic <lateman-jpeg@outlook.fr>
2021-12-22 08:50:25 +0100

    finalize shit

--- index.php
+++ index.php
@@ -9,148 +9,179 @@
 </head>
 
 <body>
-    <header>
-        <div class="top flex">
-            <div>
-                <p><a href="/">JeckSpace.Cum</a> &nbsp;|&nbsp; <a href="/">Home</a></p>
-            </div>
-            <div class="right flex">
-                <p>Search JeckSpace:&nbsp;</p>
-                <input type="text">
-                &nbsp;&nbsp;&nbsp;
-                <button>Go</button>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <p><strong><a href="#">Help</a></strong> | <strong><a href="#">Sign Up</a></strong>
-                </p>
-            </div>
-        </div>
-        <div class="logo-container">
-            <img src="img/logo.png" alt="">
-        </div>
-    </header>
-    <nav class="navbar">
-        <a href="#">Home</a>
-        | <a href="#">Browse</a>
-        | <a href="#">Search</a>
-        | <a href="#">Invite</a>
-        | <a href="#">Rank</a>
-        | <a href="#">Mail</a>
-        | <a href="#">Blog</a>
-        | <a href="#">Favorites</a>
-        | <a href="#">Forum</a>
-        | <a href="#">Groups</a>
-        | <a href="#">Games</a>
-        | <a href="#">Music</a>
-        | <a href="#">Classifieds</a>
-        | <a href="#">SignUp</a>
-    </nav>
-    <main>
-        <div class="mgb">
-            <section class="left">
-                <div class="featured">
-                    <div class="title">
-                        <p>Cool New People</p>
-                    </div>
-                    <div class="content imgtable">
-                        <div>
-                        <a href="#"><p>Nikki</p><img src="amogus.jpg" alt=""></a>
-                        </div>
-                        <div>
-                        <a href="#"><p>Haley</p><img src="amogus.jpg" alt=""></a>
-                        </div>
-                        <div>
-                        <a href="#"><p>Leiah</p><img src="amogus.jpg" alt=""></a>
-                        </div>
-                        <div>
-                            <p><a href="#">Rachel</a></p><img src="amogus.jpg" alt="">
-                        </div>
-                    </div>
+    <div class="bbb">
+        <header>
+            <div class="top flex">
+                <div>
+                    <p><a href="/">JeckSpace.Cum</a> &nbsp;|&nbsp; <a href="/">Home</a></p>
                 </div>
-                <div class="featured blue">
-                    <div class="title">
-                        <p>MySpace Member Blogs</p>
-                        <p class="fa-ri"><a href="#">[what's this?]</a></p>
-                    </div>
-                    <div class="content txttable">
-                        <div class="left">
-                            <p>02:01a - <a href="#">KadeJoe</a></p>
-                            <p>02:01a - <a href="#">KadeJoe</a></p>
-                            <p>02:01a - <a href="#">KadeJoe</a></p>
-                            <p>02:01a - <a href="#">KadeJoe</a></p>
-                            <p>02:01a - <a href="#">KadeJoe</a></p>
-                            <p>02:01a - <a href="#">KadeJoe</a></p>
-                        </div>
-                        <div class="right">
-                            <p>02:01a - <a href="#">KadeJoe</a></p>
-                            <p>02:01a - <a href="#">KadeJoe</a></p>
-                            <p>02:01a - <a href="#">KadeJoe</a></p>
-                            <p>02:01a - <a href="#">KadeJoe</a></p>
-                            <p>02:01a - <a href="#">KadeJoe</a></p>
-                            <p>02:01a - <a href="#">KadeJoe</a></p>
-                        </div>
-                    </div>
+                <div class="right flex">
+                    <p>Search JeckSpace:&nbsp;</p>
+                    <input type="text">
+                    &nbsp;&nbsp;&nbsp;
+                    <button>Go</button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <p><strong><a href="#">Help</a></strong> | <strong><a href="#">Sign Up</a></strong>
+                    </p>
                 </div>
+            </div>
+            <div class="logo-container">
+                <img src="img/logo.png" alt="">
+            </div>
+        </header>
+        <nav>
+            <a href="#">Home</a>
+            | <a href="#">Browse</a>
+            | <a href="#">Search</a>
+            | <a href="#">Invite</a>
+            | <a href="#">Rank</a>
+            | <a href="#">Mail</a>
+            | <a href="#">Blog</a>
+            | <a href="#">Favorites</a>
+            | <a href="#">Forum</a>
+            | <a href="#">Groups</a>
+            | <a href="#">Games</a>
+            | <a href="#">Music</a>
+            | <a href="#">Classifieds</a>
+            | <a href="#">SignUp</a>
+        </nav>
+        <main>
+            <div class="mgb">
+                <section class="left">
+                    <div class="featured">
+                        <div class="title">
+                            <p>Cool New People</p>
+                        </div>
+                        <div class="content imgtable">
+                            <div>
+                                <a href="#">
+                                    <p>Nikki</p><img src="amogus.jpg" alt="">
+                                </a>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <p>Haley</p><img src="amogus.jpg" alt="">
+                                </a>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <p>Leiah</p><img src="amogus.jpg" alt="">
+                                </a>
+                            </div>
+                            <div>
+                                <p><a href="#">Rachel</a></p><img src="amogus.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="featured blue">
+                        <div class="title">
+                            <p>MySpace Member Blogs</p>
+                            <p class="fa-ri"><a href="#">[what's this?]</a></p>
+                        </div>
+                        <div class="content txttable">
+                            <div class="left">
+                                <p>02:01a - <a href="#">KadeJoe</a></p>
+                                <p>02:01a - <a href="#">KadeJoe</a></p>
+                                <p>02:01a - <a href="#">KadeJoe</a></p>
+                                <p>02:01a - <a href="#">KadeJoe</a></p>
+                                <p>02:01a - <a href="#">KadeJoe</a></p>
+                                <p>02:01a - <a href="#">KadeJoe</a></p>
+                            </div>
+                            <div class="right">
+                                <p>02:01a - <a href="#">KadeJoe</a></p>
+                                <p>02:01a - <a href="#">KadeJoe</a></p>
+                                <p>02:01a - <a href="#">KadeJoe</a></p>
+                                <p>02:01a - <a href="#">KadeJoe</a></p>
+                                <p>02:01a - <a href="#">KadeJoe</a></p>
+                                <p>02:01a - <a href="#">KadeJoe</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="right">
+                    <div class="login-form">
+                        <h4>Member Login</h4>
+                        <form>
+                            <label for="email">E-Mail:</label>
+                            <input type="email" name="email">
+                            <br>
+                            <br>
+                            <label for="password">Password:</label>
+                            <input type="password" name="password">
+                            <br>
+                            <br>
+                            <input type="checkbox" name="remember">
+                            <label for="remember">Remember my E-mail</label>
+                            <br>
+                            <br>
+                            <div class="btns">
+                                <input type="submit" value="LOGIN">
+                                <button class="org">SIGN UP!</button>
+                            </div>
+                            <a href="#">Forgot your password?</a>
+                    </div>
+                    <div class="ad">
+                        <img src="img/advertisement.gif" alt="ADVERTISSEMENT">
+                    </div>
+                    </form>
+            </div>
             </section>
-            <section class="right">
-                <div class="login-form">
-                    <h4>Member Login</h4>
-                    <form>
-                        <label for="email">E-Mail:</label>
-                        <input type="email" name="email">
-                        <br>
-                        <br>
-                        <label for="password">Password:</label>
-                        <input type="password" name="password">
-                        <br>
-                        <br>
-                        <input type="checkbox" name="remember">
-                        <label for="remember">Remember my E-mail</label>
-                        <br>
-                        <br>
-                        <div class="btns">
-                            <input type="submit" value="LOGIN">
-                            <button class="org">SIGN UP!</button>
-                        </div>
-                        <a href="#">Forgot your password?</a>
+            <div class="news">
+                <div>
+                    <section>
+                        <h3>Get Started on JeckSpace!</h3>
+                        <p>Join for free, and view profiles, connect with others, blog, rank music, and much more!</p>
+                    </section>
+                    <p class="mlink"><a href="#">Learn More</a></p>
                 </div>
-                <div class="ad">
-                    <img src="img/advertisement.gif" alt="ADVERTISSEMENT">
+                <div>
+                    <section>
+                        <h3>Create Your Profile!</h3>
+                        <p>Tell us about yourself, upload your pictures, and start adding triends to your network!</p>
+                    </section>
+                    <p class="mlink"><a href="#">Start Now</a></p>
                 </div>
-                </form>
-        </div>
-        </section>
-        </div>
-        <div class="news">
-            <div>
-                <section>
-                    <h3>Get Started on JeckSpace!</h3>
-                    <p>Join for free, and view profiles, connect with others, blog, rank music, and much more!</p>
-                </section>
-                <p class="mlink"><a href="#">Learn More</a></p>
+                <div>
+                    <section>
+                        <h3>Browse through Profiles!</h3>
+                        <p>Read through millions of profiles on JeckSpace! See pix, read blogs, and more!</p>
+                    </section>
+                    <p class="mlink"><a href="#">Browse Now</a></p>
+                </div>
+                <div>
+                    <section>
+                        <h3>Invite Your Triends!</h3>
+                        <p>Invite your triends, and as they invite their triends your network will grow even larger!</p>
+                    </section>
+                    <p class="mlink"><a href="#">Invite Triends Now</a></p>
+                </div>
             </div>
-            <div>
-                <section>
-                    <h3>Create Your Profile!</h3>
-                    <p>Tell us about yourself, upload your pictures, and start adding triends to your network!</p>
-                </section>
-                <p class="mlink"><a href="#">Start Now</a></p>
-            </div>
-            <div>
-                <section>
-                    <h3>Browse through Profiles!</h3>
-                    <p>Read through millions of profiles on JeckSpace! See pix, read blogs, and more!</p>
-                </section>
-                <p class="mlink"><a href="#">Browse Now</a></p>
-            </div>
-            <div>
-                <section>
-                    <h3>Invite Your Triends!</h3>
-                    <p>Invite your triends, and as they invite their triends your network will grow even larger!</p>
-                </section>
-                <p class="mlink"><a href="#">Invite Triends Now</a></p>
-            </div>
-        </div>
+    </div>
     </main>
+    </div>
+    <div class="bg">
+        <div class="bbb">
+            <footer>
+                <div class="flex fce">
+                    <img src="img/truste.gif" alt="TRUSTe">
+                    &nbsp;
+                    <p>JeckSpace is <strong>NOT</strong> a licensee of the TRUSTe Privacy Program</p>
+                </div>
+                <nav class="trans">
+                    <a href="#">About</a>
+                    | <a href="#">News</a>
+                    | <a href="#">FAQ</a>
+                    | <a href="#">Terms</a>
+                    | <a href="#">Privacy</a>
+                    | <a href="#">Contact</a>
+                    | <a href="#">Promote</a>
+                    | <a href="#">Advertise</a>
+                    | <a href="#">JeckSpace Shop</a>
+                </nav>
+                <p class="lt">©<?php echo date("Y"); ?> JeckSpace.Cum. All Rights Reserved.</p>
+            </footer>
+        </div>
+    </div>
 </body>
 
 </html>
